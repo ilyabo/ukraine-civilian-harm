29975aadd732187767116da1a4b890e6d587911a
Ilya Boyandin <ilya@boyandin.me>
2022-05-03 20:58:33 +0200

    Data updated

--- src/Map.svelte
+++ src/Map.svelte
@@ -4,7 +4,7 @@
   import { mapbox, key } from './mapbox.js';
   import * as d3 from 'd3';
 
-  const INCIDENTS_URL = '/data/ukr-civharm-2022-05-01.json';
+  const INCIDENTS_URL = '/data/ukr-civharm-2022-05-03.json';
   const COUNTRY_OUTLINE_URL = '/data/country-outline.json';
 
   setContext(key, {
